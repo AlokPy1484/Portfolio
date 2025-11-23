d60aa1a76d6698bb9ce7a23b19d848ce01759a71
AlokPy1484 <alokpandey1483@gmail.com>
2025-11-23 13:13:04 +0530

    added scroll to contact(scroll to end)

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,6 +1,17 @@
 import { Cover } from "@/components/ui/cover";
 
+
 function Navbar(){
+
+
+    const scrollToContact = () => {
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: "smooth"
+        })
+    }
+
+
 
     return(
         <div className="fixed top-0 w-screen h-20 flex flex-row justify-center md:justify-between items-center md:px-30 backdrop-blur-xs text-white md:text-2xl z-100">
@@ -9,7 +20,7 @@ function Navbar(){
                 <div>Home</div>
                 <div>About</div>
                 <div>Tech Stack</div>
-                <div><Cover >Contact</Cover></div>
+                <div onClick={scrollToContact}><Cover >Contact</Cover></div>
             </div>
         </div>
     )
