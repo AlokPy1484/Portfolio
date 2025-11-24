374bda3e2274204c2ecd87e11ec9195d6daa671d
AlokPy1484 <alokpandey1483@gmail.com>
2025-11-24 17:07:27 +0530

    added resume linking

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -11,16 +11,28 @@ function Navbar(){
         })
     }
 
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: document.body.scrollTop,
+            behavior: "smooth"
+        })
+    }
+
+
+    const openResume = () => {
+        window.open('/Resume.pdf', '_blank')
+    }
+
 
 
     return(
         <div className="fixed top-0 w-screen h-20 flex flex-row justify-center md:justify-between items-center md:px-30 backdrop-blur-xs text-white md:text-2xl z-100">
             <div className="hidden md:block">Portfolio</div>
             <div className="flex flex-row justify-center items-center h-10 gap-10">
-                <div>Home</div>
+                <div onClick={scrollToTop}>Home</div>
                 <div>About</div>
-                <div>Tech Stack</div>
-                <div onClick={scrollToContact}><Cover >Contact</Cover></div>
+                <div onClick={openResume} className="cursor-pointer">Resume</div>
+                <div onClick={scrollToContact} className="cursor-pointer"><Cover >Contact</Cover></div>
             </div>
         </div>
     )
